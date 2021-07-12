c5b36d58215155c7bee93e5b50eadd2628fce7bd
Jonathan Beck <beckjonathan94@gmail.com>
2021-07-12 19:12:15 +0200

    Add assets

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
     <div id="app">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <!-- header -->
+        <!-- banner -->
+        <!-- headline -->
+        <!-- crew -->
+        <!-- footer -->
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+        // HelloWorld
     }
 }
 </script>
