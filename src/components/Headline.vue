f32dff917b8abcef2f3754ebf078186571eb4fb9
Jonathan Beck <beckjonathan94@gmail.com>
2021-07-14 19:10:51 +0200

    Add headlines and deadlines

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -1,15 +1,67 @@
   <template>
     <div id="headline">
-        <div class="container">
-           <div class="w-25">headline 1</div>
-           <div class="w-25">headline 2</div>
-           <div class="w-50">map 1</div>
-           <div class="w-50">big picture</div>
-           <div class="w-50">map 2</div>
-           <div class="w-25">flag picture</div>
-           <div class="w-25">small picture</div>
-           <div class="w-50">news bilder</div>
-           <div class="w-50">segelteam</div>
+        <div class="container-fluid p-0">
+            <div class="row no-gutters">
+                <div class="col-sm-12 col-md-6 row no-gutters" id="headlines">
+                    <div class="col-sm-12 col-md-6 bg-body">
+                        <div class="tile news-headline-container">
+                            <div class="date">
+                                <img src="/images/icons/calendar-grey.png" class="mr-2"/>
+                                <span class="text-grey">23 mai 2021</span>
+                            </div>
+                            <div class="news-headline">News headline</div>
+                            <p>Lorem Ipsum. Proin gravida nibh velit auctor aliquet. Aenean sollicitudin.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 bg-black">
+                        <div class="tile news-headline-container">
+                            <div class="date">
+                                <img src="/images/icons/calendar-grey.png" class="mr-2"/>
+                                <span class="text-grey">23 mai 2021</span>
+                            </div>
+                            <div class="news-headline">News headline</div>
+                            <p class="text-white">Lorem Ipsum. Proin gravida nibh velit auctor aliquet. Aenean sollicitudin.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="tile calendar-map" id="image-map-1">
+                        <div class="date">17 mai</div>
+                        <div class="date-title">headline beitrag 2021</div>
+                        <div class="date-subtitle">subtitle</div>
+                        <p>Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-sm-12 col-md-6">
+                    <div class="tile" id="image-boat"></div>
+                </div>
+                <div class="col-sm-12 col-md-6 row no-gutters">
+                    <div class="col-sm-12">
+                        <div class="tile calendar-map" id="image-map-2">
+                            <div class="date">03 okt</div>
+                            <div class="date-title">headline beitrag 2021</div>
+                            <div class="date-subtitle">subtitle ort - jahr</div>
+                            <p>Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <div class="tile" id="image-flag"></div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <div class="tile" id="image-boat-small"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-sm-12 col-md-6">
+                    <div class="tile">news bilder</div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="tile">segelteam</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
